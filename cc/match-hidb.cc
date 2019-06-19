65b2fd48e6d3685cb51a754b80d646166121b642
Eugene Skepner <github@skepner.eu>
2019-06-19 11:13:02 +0200

    match hidb (in progress)

--- cc/match-hidb.cc
+++ cc/match-hidb.cc
@@ -1,3 +1,4 @@
+#include "acmacs-base/string-matcher.hh"
 #include "hidb-5/hidb.hh"
 #include "seqdb-3/match-hidb.hh"
 #include "seqdb-3/fasta.hh"
@@ -12,8 +13,31 @@ struct hidb_ref_t
     std::vector<lab_id_index_entry_t> lab_id_index;
 };
 
+struct score_size_t
+{
+    string_match::score_t score;
+    size_t len;
+
+    constexpr bool operator<(const score_size_t& a) const { return score < a.score; }
+};
+
+struct score_seq_found_t : public score_size_t
+{
+    size_t seq_no;
+    size_t found_no;
+
+    score_seq_found_t(const score_size_t& ss, size_t sn, size_t fn) : score_size_t{ss.score, ss.len}, seq_no{sn}, found_no{fn} {}
+    constexpr bool operator<(const score_seq_found_t& a) const { return score > a.score; }
+};
+
+using Matching = std::vector<std::vector<score_seq_found_t>>;
+
+static bool match(const hidb_ref_t& hidb_ref, const acmacs::seqdb::v3::sequence_t& seq, std::string_view subtype);
 static void find_by_lab_id(hidb::AntigenPList& found, const hidb_ref_t& hidb_ref, const acmacs::seqdb::v3::sequence_t& sequence);
 static void find_by_name(hidb::AntigenPList& found, const hidb_ref_t& hidb_ref, const acmacs::seqdb::v3::sequence_t& sequence);
+static Matching make_matching(const acmacs::seqdb::v3::sequence_t& seq, const hidb::AntigenPList& found);
+static bool match_greedy(const acmacs::seqdb::v3::sequence_t& seq, const hidb::AntigenPList& found, const Matching& matching);
+static bool match_normal(const acmacs::seqdb::v3::sequence_t& seq, const hidb::AntigenPList& found, const Matching& matching);
 
 // ----------------------------------------------------------------------
 
@@ -25,30 +49,71 @@ void acmacs::seqdb::v3::match_hidb(std::vector<fasta::scan_result_t>& sequences)
         hidbs.emplace(std::string{subtype}, hidb_ref_t{hidb_antigens, hidb_antigens->sorted_by_labid()});
     }
 
+    size_t matched = 0;
     for (auto& en : sequences) {
         auto& seq = en.sequence;
         if (const auto hb = seq.type_subtype().h_or_b(); hb == "B" || hb == "H1" || hb == "H3") {
-            const auto& hidb_ref = hidbs.find(hb)->second;
-            hidb::AntigenPList found_hidb_antigens;
-            find_by_lab_id(found_hidb_antigens, hidb_ref, seq);
-            // find_by_name(found_hidb_antigens, hidb_ref, seq);
-            std::sort(found_hidb_antigens.begin(), found_hidb_antigens.end());
-            found_hidb_antigens.erase(std::unique(found_hidb_antigens.begin(), found_hidb_antigens.end()), found_hidb_antigens.end());
-            if (!found_hidb_antigens.empty()) {
-                // update country, continent?
-                // check lineage
-                if (hb == "B") {
-                    if (const auto hidb_lineage = found_hidb_antigens.front()->lineage(); hidb_lineage != acmacs::chart::BLineage::Unknown && hidb_lineage != *seq.lineage())
-                        fmt::print(stderr, "WARNING: lineage mismatch seq: {} vs. hidb: {} {}\n", seq.full_name(), found_hidb_antigens.front()->name(), static_cast<std::string>(found_hidb_antigens.front()->lineage()));
-                }
-                // update date
-                // for (const auto& en: found_hidb_antigens)
-                //     seq.add_date(en->date());
-            }
+            if (match(hidbs.find(hb)->second, seq, hb))
+                ++matched;
         }
     }
+    fmt::print("INFO: matched against hidb: {}\n", matched);
 
 } // acmacs::seqdb::v3::match_hidb
+
+// ----------------------------------------------------------------------
+
+bool match(const hidb_ref_t& hidb_ref, const acmacs::seqdb::v3::sequence_t& seq, std::string_view subtype)
+{
+    hidb::AntigenPList found_hidb_antigens;
+    find_by_lab_id(found_hidb_antigens, hidb_ref, seq);
+    find_by_name(found_hidb_antigens, hidb_ref, seq);
+    std::sort(found_hidb_antigens.begin(), found_hidb_antigens.end());
+    found_hidb_antigens.erase(std::unique(found_hidb_antigens.begin(), found_hidb_antigens.end()), found_hidb_antigens.end());
+
+    bool matched = false;
+    if (!found_hidb_antigens.empty()) {
+        // update country, continent?
+        if (subtype == "B") {
+            if (const auto hidb_lineage = found_hidb_antigens.front()->lineage(); hidb_lineage != acmacs::chart::BLineage::Unknown && hidb_lineage != *seq.lineage())
+                fmt::print(stderr, "WARNING: lineage mismatch seq: {} vs. hidb: {} {}\n", seq.full_name(), found_hidb_antigens.front()->name(),
+                           static_cast<std::string>(found_hidb_antigens.front()->lineage()));
+        }
+        // update date
+        // for (const auto& en: found_hidb_antigens)
+        //     seq.add_date(en->date());
+
+        const auto matching = make_matching(seq, found_hidb_antigens); // for each seq list of matching [[score, min passage len], found_no] - sorted by score desc
+        matched = match_greedy(seq, found_hidb_antigens, matching);
+        // matched = match_normal(seq, found, matching);
+    }
+    return matched;
+
+} // match
+
+// ----------------------------------------------------------------------
+
+Matching make_matching(const acmacs::seqdb::v3::sequence_t& seq, const hidb::AntigenPList& found)
+{
+    Matching result;
+    return result;
+
+} // make_matching
+
+// ----------------------------------------------------------------------
+
+bool match_greedy(const acmacs::seqdb::v3::sequence_t& seq, const hidb::AntigenPList& found, const Matching& matching)
+{
+    return false;
+} // match_greedy
+
+// ----------------------------------------------------------------------
+
+bool match_normal(const acmacs::seqdb::v3::sequence_t& seq, const hidb::AntigenPList& found, const Matching& matching)
+{
+    return false;
+
+} // match_normal
 
 // ----------------------------------------------------------------------
 
