70ec506762b77054c291957f0cbebbea3afa22d6
Eugene Skepner <github@skepner.eu>
2019-07-18 12:50:28 +0200

    in_json development

--- cc/seqdb-parse.cc
+++ cc/seqdb-parse.cc
@@ -1,6 +1,3 @@
-#include <stack>
-#include <memory>
-
 #include "acmacs-base/in-json.hh"
 #include "seqdb-3/seqdb-parse.hh"
 #include "seqdb-3/seqdb.hh"
@@ -9,125 +6,89 @@
 
 namespace local
 {
-    class error : public std::runtime_error
-    {
-      public:
-        // template <typename S> error(S&& message) : std::runtime_error(fmt::format("error: {}", message)) {}
-        error(std::string_view m1) : std::runtime_error{fmt::format("seqdb parsing error: {}", m1)} {}
-        error(std::string_view m1, std::string_view m2) : error(fmt::format("{}{}", m1, m2)) {}
-    };
-
-    class stack_entry
-    {
-      public:
-        stack_entry() = default;
-        stack_entry(const stack_entry&) = default;
-        virtual ~stack_entry() = default;
-        virtual const char* injson_name() = 0;
-        virtual std::unique_ptr<stack_entry> injson_put_object() = 0;
-        virtual void injson_put_string(std::string_view data) { key_ = data; }
-        virtual void injson_put_integer(std::string_view /*data*/) { throw error("stack_entry::injson_put_integer"); }
-        // virtual void injson_put_real(double /*data*/) { throw error("stack_entry::injson_put_real"); }
-        virtual void injson_put_array() { throw error("stack_entry::injson_put_array"); }
-        virtual void injson_pop_array() { throw error("stack_entry::injson_pop_array"); }
-
-      protected:
-        std::string_view key_{};
-
-        void reset_key() { key_ = std::string_view{}; }
-
-    };
-
-    class labs : public stack_entry
+    class labs : public in_json::stack_entry
     {
       public:
         labs(acmacs::seqdb::SeqdbSeq::labs_t& target) : target_{target} {}
         const char* injson_name() override { return "labs"; }
-        std::unique_ptr<stack_entry> injson_put_object() override { throw error("labs: unexpected subobject"); }
         void injson_put_array() override {}
-        void injson_pop_array() override {}
 
-        void injson_put_string(std::string_view data) override
+        void injson_put_key(std::string_view data) override
         {
-            if (key_.empty()) {
-                stack_entry::injson_put_string(data);
-                target_.emplace_back(data, acmacs::seqdb::SeqdbSeq::lab_ids_t{});
-            }
-            else
-                target_.back().second.emplace_back(data);
+            in_json::stack_entry::injson_put_key(data);
+            target_.emplace_back(data, acmacs::seqdb::SeqdbSeq::lab_ids_t{});
         }
+
+        void injson_put_string(std::string_view data) override { target_.back().second.emplace_back(data); }
 
       private:
         acmacs::seqdb::SeqdbSeq::labs_t& target_;
     };
 
-    class seq : public stack_entry
+    class seq : public in_json::stack_entry
     {
       public:
         seq(acmacs::seqdb::SeqdbSeq& target) : target_{target} {}
 
         const char* injson_name() override { return "seq"; }
 
-        std::unique_ptr<stack_entry> injson_put_object() override
+        std::unique_ptr<in_json::stack_entry> injson_put_object() override
         {
             // if (key_[0] == 'l') {
             reset_key();
             return std::make_unique<labs>(target_.lab_ids);
             // }
             // else
-            //     throw error("seq: unexpected sub-object, key: ", key_);
+            //     throw in_json::parse_error("seq: unexpected sub-object, key: ", key_);
         }
 
         void injson_put_array() override
         {
             // if (key_.size() != 1 || (key_[0] != 'p' && key_[0] != 'c' && key_[0] != 'h' && key_[0] != 'r'))
-            //     throw error("seq: unexpected array, key: ", key_);
+            //     throw in_json::parse_error("seq: unexpected array, key: ", key_);
         }
 
         void injson_pop_array() override
         {
             // if (key_.size() != 1 || (key_[0] != 'p' && key_[0] != 'c' && key_[0] != 'h' && key_[0] != 'r'))
-            //     throw error("seq: unexpected array, key: ", key_);
+            //     throw in_json::parse_error("seq: unexpected array, key: ", key_);
             reset_key();
         }
 
         void injson_put_string(std::string_view data) override
         {
-            if (key_.empty())
-                stack_entry::injson_put_string(data);
-            else
-                // else if (key_.size() == 1) {
-                switch (key_[0]) {
-                    case 'p':
-                        target_.passages.emplace_back(data);
-                        break;
-                    case 'r':
-                        target_.reassortants.emplace_back(data);
-                        break;
-                    case 'c':
-                        target_.clades.emplace_back(data);
-                        break;
-                    case 'h':
-                        target_.hi_names.emplace_back(data);
-                        break;
-                    // case 'g':
-                    //     target_.gene = data;
-                    //     reset_key();
-                    //     break;
-                    case 'a':
-                        target_.amino_acids = data;
-                        reset_key();
-                        break;
-                    case 'n':
-                        target_.nucs = data;
-                        reset_key();
-                        break;
-                    default:
-                        throw error("seq: unexpected key: ", data);
-                }
+            // else if (key_.size() == 1) {
+            switch (key_[0]) {
+                case 'p':
+                    target_.passages.emplace_back(data);
+                    break;
+                case 'r':
+                    target_.reassortants.emplace_back(data);
+                    break;
+                case 'c':
+                    target_.clades.emplace_back(data);
+                    break;
+                case 'h':
+                    target_.hi_names.emplace_back(data);
+                    break;
+                // case 'g':
+                //     target_.gene = data;
+                //     reset_key();
+                //     break;
+                case 'a':
+                    target_.amino_acids = data;
+                    reset_key();
+                    break;
+                case 'n':
+                    target_.nucs = data;
+                    reset_key();
+                    break;
+                default:
+                    throw in_json::parse_error("seq: unexpected key: ", data);
+            }
             // }
             // else
-            //     throw error("seq: unexpected key: ", data);
+            //     throw in_json::parse_error("seq: unexpected key: ", data);
         }
 
         void injson_put_integer(std::string_view data) override
@@ -143,25 +104,26 @@ namespace local
                     reset_key();
                     break;
                 default:
-                    throw error("seq: unexpected integer, key: ", key_);
+                    in_json::stack_entry::injson_put_integer(data);
+                    break;
             }
             // }
             // else
-            //     throw error("seq: unexpected integer, key: ", key_);
+            //     in_json::stack_entry::injson_put_integer(data);
         }
 
       private:
         acmacs::seqdb::SeqdbSeq& target_;
     };
 
-    class entry : public stack_entry
+    class entry : public in_json::stack_entry
     {
       public:
         entry(acmacs::seqdb::SeqdbEntry& target) : target_{target} {}
 
         const char* injson_name() override { return "entry"; }
 
-        std::unique_ptr<stack_entry> injson_put_object() override
+        std::unique_ptr<in_json::stack_entry> injson_put_object() override
         {
             return std::make_unique<seq>(target_.seqs.emplace_back()); // objects are unly under "s"
         }
@@ -170,81 +132,76 @@ namespace local
         {
             // fmt::print(stderr, "WARNING: entry::injson_put_array\n");
             // if (key_.size() != 1 || (key_[0] != 'd' && key_[0] != 's'))
-            //     throw error("entry: unexpected array, key: " + std::string(key_));
+            //     throw in_json::parse_error("entry: unexpected array, key: " + std::string(key_));
         }
 
         void injson_pop_array() override
         {
             // fmt::print(stderr, "WARNING: entry::injson_pop_array\n");
             // if (key_.size() != 1 || (key_[0] != 'd' && key_[0] != 's'))
-            //     throw error("entry: unexpected array, key: " + std::string(key_));
+            //     throw in_json::parse_error("entry: unexpected array, key: " + std::string(key_));
             reset_key();
         }
 
         void injson_put_string(std::string_view data) override
         {
-            if (key_.empty())
-                stack_entry::injson_put_string(data);
-            else
-                switch (key_[0]) {
-                    case 'N':
-                        target_.name = data;
-                        reset_key();
-                        // fmt::print(stderr, "NAME: {}\n", name_);
-                        break;
-                    case 'C':
-                        target_.continent = data;
-                        reset_key();
-                        break;
-                    case 'c':
-                        target_.country = data;
-                        reset_key();
-                        break;
-                    case 'd':
-                        target_.dates.emplace_back(data);
-                        break;
-                    case 'l':
-                        target_.lineage = data;
-                        reset_key();
-                        break;
-                    case 'v':
-                        target_.virus_type = data;
-                        reset_key();
-                        break;
-                    default:
-                        throw error("entry: unexpected key: ", data);
-                }
+            switch (key_[0]) {
+                case 'N':
+                    target_.name = data;
+                    reset_key();
+                    // fmt::print(stderr, "NAME: {}\n", name_);
+                    break;
+                case 'C':
+                    target_.continent = data;
+                    reset_key();
+                    break;
+                case 'c':
+                    target_.country = data;
+                    reset_key();
+                    break;
+                case 'd':
+                    target_.dates.emplace_back(data);
+                    break;
+                case 'l':
+                    target_.lineage = data;
+                    reset_key();
+                    break;
+                case 'v':
+                    target_.virus_type = data;
+                    reset_key();
+                    break;
+                default:
+                    throw in_json::parse_error("entry: unexpected key: ", data);
+            }
             // }
             // else
-            //     throw error("entry: unexpected key: ", data);
+            //     throw in_json::parse_error("entry: unexpected key: ", data);
         }
 
       private:
         acmacs::seqdb::SeqdbEntry& target_;
     };
 
-    class db : public stack_entry
+    class db : public in_json::stack_entry
     {
       public:
         db(std::vector<acmacs::seqdb::SeqdbEntry>& entries) : entries_{entries} {}
 
         const char* injson_name() override { return "db"; }
 
-        std::unique_ptr<stack_entry> injson_put_object() override { return std::make_unique<entry>(entries_.emplace_back()); }
+        std::unique_ptr<in_json::stack_entry> injson_put_object() override { return std::make_unique<entry>(entries_.emplace_back()); }
 
         void injson_put_string(std::string_view data) override
         {
             if (key_ == "  version") {
                 if (data != "sequence-database-v2")
-                    throw error("unsupported version: ", data);
+                    throw in_json::parse_error("unsupported version: ", data);
                 reset_key();
             }
-            else if (key_.empty())
-                stack_entry::injson_put_string(data);
             else if (key_ == "  date" || key_ == "_")
                 reset_key();
             else
-                throw error("unsupported field: ", data);
+                throw in_json::parse_error("unsupported field: ", data);
         }
 
         void injson_put_array() override {}
@@ -254,30 +211,7 @@ namespace local
         std::vector<acmacs::seqdb::v3::SeqdbEntry>& entries_;
     };
 
-    class sink
-    {
-      public:
-        sink(std::vector<acmacs::seqdb::SeqdbEntry>& entries) : entries_{entries} {}
-
-        void injson_object_start()
-        {
-            if (target_.empty())
-                target_.push(std::make_unique<db>(entries_));
-            else
-                target_.push(target_.top()->injson_put_object());
-        }
-
-        void injson_object_end() { target_.pop(); }
-        void injson_array_start() { target_.top()->injson_put_array(); }
-        void injson_array_end() { target_.top()->injson_pop_array(); }
-        template <typename Iter> void injson_string(Iter first, Iter last) { target_.top()->injson_put_string({&*first, static_cast<size_t>(last - first)}); }
-        template <typename Iter> void injson_integer(Iter first, Iter last) { target_.top()->injson_put_integer({&*first, static_cast<size_t>(last - first)}); }
-        template <typename Iter> void injson_real(Iter /*first*/, Iter /*last*/) {}
-
-      private:
-        std::vector<acmacs::seqdb::v3::SeqdbEntry>& entries_;
-        std::stack<std::unique_ptr<stack_entry>> target_;
-    };
+    using sink = in_json::object_sink<std::vector<acmacs::seqdb::SeqdbEntry>, db>;
 
 } // namespace local
 
