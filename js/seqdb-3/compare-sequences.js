3a048b262daf2efeae8f7cfefc95406406d4e411
Eugene Skepner <github@skepner.eu>
2020-06-01 17:30:21 +0200

    compare sequences web interface

--- js/seqdb-3/compare-sequences.js
+++ js/seqdb-3/compare-sequences.js
@@ -9,31 +9,59 @@ function main()
 
 function show_full_sequences()
 {
-    const add_seqence = function(tr, seq) {
-        for (let aa of seq) {
+    const add_seqence = function(tr, seq_s) {
+        const seq = [...seq_s];
+        seq.forEach(function(aa, pos0) {
             const aa_td = document.createElement("td");
             aa_td.classList.add(`aa${aa}`);
+            aa_td.classList.add("aa");
+            if (pos0 % 10 == 0)
+                aa_td.classList.add("sep-left-one");
+            else if (pos0 % 10 == 5)
+                aa_td.classList.add("sep-left-six");
             aa_td.innerHTML = aa;
-            tr.appendChild(aa_td)
-        }
+            tr.appendChild(aa_td);
+        });
     };
-    
+
+    const add_ruler = function() {
+        const tr = document.createElement("tr");
+        tr.classList.add("aa-ruler");
+        tr.innerHTML = `<td colspan="2"></td>`;
+        for (let pos1 = 1; pos1 < 550; ++pos1) {
+            const td = document.createElement("td");
+            if (pos1 % 10 == 1) {
+                td.classList.add("sep-left-one");
+                let pos_s = "" + pos1;
+                td.innerHTML = pos_s;
+                td.setAttribute("colspan", pos_s.length);
+                pos1 += pos_s.length - 1;
+            }
+            else if (pos1 % 10 == 6) {
+                td.classList.add("sep-left-six");
+                td.innerHTML = ".";
+            }
+            else
+                td.innerHTML = ".";
+            tr.appendChild(td);
+        }
+        return tr;
+    };
+
     const tab1 = document.createElement("table");
     for (let group of compare_sequences_data.groups) {
-        const tr_group_name = document.createElement("tr");
-        tr_group_name.innerHTML = `<td class="group-name" xrowspan="${group.seq.length}">${group.name}</td><td class="seq-id"></td><td class="seq-aas"><table><tr></tr></table></td>`;
-        tr_group_name.querySelector(".seq-id").innerHTML = group.seq[0].id;
-        add_seqence(tr_group_name.querySelector(".seq-aas tr"), group.seq[0].seq);
-        tab1.appendChild(tr_group_name);
-        
-        // const members = tr1.querySelector(".group-members");
-        // for (let seq of group.seq) {
-        //     const tr2 = document.createElement("tr");
-        //     tr2.innerHTML = `<td class="sequence-id">${seq.id}</td><td><table class="seq-aas"><tr></tr></table></td>`;
-        //     members.appendChild(tr2);
-        // }
-        // tab1.appendChild(tr1);
+        tab1.appendChild(add_ruler());
+        group.seq.forEach(function(id_seq, index) {
+            const tr = document.createElement("tr");
+            if (index == 0)
+                tr.innerHTML = `<td class="group-name" rowspan="${group.seq.length}">${group.name}</td><td class="seq-id">${id_seq.id}</td>`;
+            else
+                tr.innerHTML = `<td class="seq-id">${id_seq.id}</td>`;
+            add_seqence(tr, id_seq.seq);
+            tab1.appendChild(tr);
+        });
     }
+    tab1.appendChild(add_ruler());
     document.querySelector("#full-sequences").appendChild(tab1);
 }
 
