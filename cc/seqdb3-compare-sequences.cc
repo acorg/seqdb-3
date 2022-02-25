fe17c8cc8737f5e6313a715b7491fadc0c4efc9b
Eugene Skepner <github@skepner.eu>
2022-02-25 12:49:00 +0100

    minor fix

--- cc/seqdb3-compare-sequences.cc
+++ cc/seqdb3-compare-sequences.cc
@@ -65,6 +65,3 @@ int main(int argc, char* const argv[])
 }
 
 // ----------------------------------------------------------------------
-/// Local Variables:
-/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
-/// End:
