91f04b6294c95dcbb37ca65c1961b7d4073ceaef
Eugene Skepner <github@skepner.eu>
2022-03-17 14:48:48 +0100

    minor fixes

--- cc/seqdb3-chart-compare-sequences.cc
+++ cc/seqdb3-chart-compare-sequences.cc
@@ -62,6 +62,3 @@ int main(int argc, char* const argv[])
 }
 
 // ----------------------------------------------------------------------
-/// Local Variables:
-/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
-/// End:
